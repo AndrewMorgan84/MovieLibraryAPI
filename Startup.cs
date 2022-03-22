f9a797661c28bd3048ef37d0e87129b4dc427551
Andrew <andrew.morgan@e.org>
2022-03-22 16:14:51 +0000

    Added EF packages Added ApplicationDbContext Added InitialMigration

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -21,11 +22,16 @@ namespace MovieLibraryAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
             services.AddCors(options =>
             {
                 options.AddDefaultPolicy(builder =>
                 {
-                    builder.WithOrigins("http://localhost:4200/").AllowAnyMethod().AllowAnyHeader();
+                    var frontendUrl = Configuration.GetValue<string>("frontend_url");
+                    builder.WithOrigins(frontendUrl).AllowAnyMethod().AllowAnyHeader();
                 });
             });
             services.AddControllers(options => {
