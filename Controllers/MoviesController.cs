911d6daf4cc980cc41f1cdcce809cf3bbea138d4
Andrew <andrew.morgan@e.org>
2022-03-24 19:48:58 +0000

    added movie DTOs Added MovieController Added MovieController Post Action Added Movie Helper Classes

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using MovieLibraryAPI.DTOs;
+using MovieLibraryAPI.Entities;
+using MovieLibraryAPI.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieLibraryAPI.Controllers
+{
+    [ApiController]
+    [Route("api/movies")]
+    public class MoviesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+        private readonly IFileStorageService _fileStorageService;
+        private string _container = "movies";
+
+        public MoviesController(ApplicationDbContext context, IMapper mapper, IFileStorageService fileStorageService)
+        {
+            _context = context;
+            _mapper = mapper;
+            _fileStorageService = fileStorageService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromForm] MovieCreationDTO movieCreationDTO)
+        {
+            var movie = _mapper.Map<Movie>(movieCreationDTO);
+            if(movieCreationDTO.Poster != null) 
+            {
+                movie.Poster = await _fileStorageService.SaveFile(_container, movieCreationDTO.Poster);
+            }
+
+            AnnotateActorsOrder(movie);
+            _context.Add(movie);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        private void AnnotateActorsOrder(Movie movie)
+        {
+            if (movie.MoviesActors != null)
+            {
+                for (int i = 0; i < movie.MoviesActors.Count; i++)
+                {
+                    movie.MoviesActors[i].Order = i;
+                }
+            }
+        }
+    }
+}
