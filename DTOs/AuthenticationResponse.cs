f383768788d951ae17201e9a1c7d059f6120babe
Andrew <andrew.morgan@e.org>
2022-03-27 14:37:01 +0100

    added initial Accounts Controller and account DTOs

---
+++ DTOs/AuthenticationResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MovieLibraryAPI.DTOs
+{
+    public class AuthenticationResponse
+    {
+        public string Token { get; set; }
+
+        public DateTime Expiration { get; set; }
+    }
+}
