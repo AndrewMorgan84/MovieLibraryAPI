7bf2617cd38f02477edec9908d95bc7daf565477
Andrew <andrew.morgan@e.org>
2022-03-27 20:36:40 +0100

    Added Ratings controller and DTO

---
+++ DTOs/RatingDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieLibraryAPI.DTOs
+{
+    public class RatingDTO
+    {
+        [Range(1, 5)]
+        public int Rating { get; set; }
+        public int MovieId { get; set; }
+    }
+}
